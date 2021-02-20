ab3517de35b431e120de02f9dd1d2524b51ad7a7
jorge ch <jorgecalle.92@gmail.com>
2021-02-20 18:01:35 +0100

    variables de entorno para desarrollo producción

--- src/holaMundoReact.tsx
+++ src/holaMundoReact.tsx
@@ -2,15 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const currentDate: string = new Date().toDateString();  
+const apiBase: string= `${process.env.API_BASE}`;
 
 ReactDOM.render(
     <h1>Hola Mundo con React</h1>,
-    document.getElementById("root")
+    document.getElementById("holaMundoReact")
+);
+
+ReactDOM.render(
+    <span>
+        
+    </span>,
+    document.getElementById("currentDate")
 );
 
 ReactDOM.render(
     <span>
         {currentDate}
     </span>,
-    document.getElementById("root")
+    document.getElementById("currentDate")
+);
+
+ReactDOM.render(
+    <span>
+        {apiBase}
+    </span>,
+    document.getElementById("apiBase")
 );
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,13 @@
 const { merge } = require("webpack-merge");
+const Dotenv = require('dotenv-webpack');
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-  mode: "production",
-  stats: "verbose",
+    mode: "production",
+    stats: "verbose",
+    plugins: [
+        new Dotenv({
+            path: "./prod.env",
+        }),
+    ]
 });
